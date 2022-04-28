22ac39d13d4ee0c5a86374e6456b0c9c35c43964
Mwita Chacha <jonahhchacha@gmail.com>
2022-04-28 22:34:43 +0300

    Connecting backtend to 'frontend'

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def index(request):
-
-    return render(request, 'index.html')
+    challenges = Challenge.objects.all().order_by('-posted_at')
+    return render(request, 'index.html',{'challenges':challenges})
